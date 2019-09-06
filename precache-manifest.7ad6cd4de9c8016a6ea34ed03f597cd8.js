28d5971d9f3f15ad8d5a3ad6dc00755b893bc870
TiagoTi <tiago@ti2.net.br>
2019-09-05 21:34:34 -0300

    Updates

---
+++ precache-manifest.7ad6cd4de9c8016a6ea34ed03f597cd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "397a7afe3f0894ca1902e73c767f3789",
+    "url": "/learning_python/index.html"
+  },
+  {
+    "revision": "94245803ba277b0bc2c6",
+    "url": "/learning_python/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "8bad0fbaea8a5ad6c607",
+    "url": "/learning_python/static/js/2.695728ef.chunk.js"
+  },
+  {
+    "revision": "94245803ba277b0bc2c6",
+    "url": "/learning_python/static/js/main.d9cd7394.chunk.js"
+  },
+  {
+    "revision": "77d6c502b57e6265b0f0",
+    "url": "/learning_python/static/js/runtime~main.15b09650.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/learning_python/static/media/logo.5d5d9eef.svg"
+  }
+]);
