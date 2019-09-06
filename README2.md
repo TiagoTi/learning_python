ed66876e7e3285bb42a50231ad67ae9054b502ee
TiagoTi <tiago@ti2.net.br>
2019-09-05 21:59:10 -0300

    sync

---
+++ README2.md
@@ -0,0 +1,2 @@
+# learning_python
+Here's a self-guided path to learning to program using 'Test-First Teaching'.
---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Header } from 'semantic-ui-react'
+
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Header as='h1'>First Header</Header>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
